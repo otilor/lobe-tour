964f70f3fda3bd4417197c8a8983b1e797d9ece5
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2022-12-11 18:36:36 +0100

    add tailwind

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Nav from '../components/Nav'
 
 export default function Home() {
   return (
@@ -10,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        
+        <Nav />
       </main>
 
       <footer>
