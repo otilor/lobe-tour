328d85d6ab0f0ca86e4550b137935331e81b6903
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2022-12-13 11:15:25 +0100

    add font

--- components/Nav.js
+++ components/Nav.js
@@ -85,10 +85,19 @@ export default function Example() {
           
 
           <a href="#" className="text-base font-medium text-gray-500 hover:text-gray-900">
-            Pricing
+            Overview
           </a>
           <a href="#" className="text-base font-medium text-gray-500 hover:text-gray-900">
-            Docs
+            Examples
+          </a>
+          <a href="#" className="text-base font-medium text-gray-900 hover:text-gray-900">
+            Tour
+          </a>
+          <a href="#" className="text-base font-medium text-gray-500 hover:text-gray-900">
+            Blog
+          </a>
+          <a href="#" className="text-base font-medium text-gray-500 hover:text-gray-900">
+            Help
           </a>
 
           
