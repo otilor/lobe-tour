ab6a34accbd436e9ab63c9e1ab294dff75efdaea
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2022-12-15 12:36:53 +0100

    tweak UI

--- components/Nav.js
+++ components/Nav.js
@@ -199,12 +199,11 @@ export default function Example() {
       </div>
       <div className="align-items-center justify-center h-screen mx-auto">
         <div class="sm:text-center">
-          <h1 class="text-3xl font-semibold tracking-wide leading-8 text-indigo-600">
-            Lobe <span>Tour</span>
+          <h1 class="text-6xl font-bold tracking-wide leading-8">
+            Lobe <span className="text-green-400">Tour</span>
           </h1>
           <p class="mx-auto mt-6 max-w-2xl text-lg leading-8 text-gray-600">
-            Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam
-            voluptatum cupiditate veritatis in accusamus quisquam.
+            Build your first machine learning model in ten minutes. No code or experience required.
           </p>
         </div>
       </div>
